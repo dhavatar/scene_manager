94740fb6976665c0ffc1d4db07ef398f88522946
David Hsu <dahsboot@gmail.com>
2025-01-22 13:43:47 -0800

    SceneResource returns the enum on demand instead of storing it

--- addons/scene_manager/scene_resource.gd
+++ addons/scene_manager/scene_resource.gd
@@ -4,18 +4,21 @@ extends Resource
 ## Custom resource for the editor property to allow it to have a custom inspector.
 
 @export var string_value: String
-@export var scene_value: Scenes.SceneName = Scenes.SceneName.NONE
+
+## Accessor to get the enum from the string value in this resource.
+var scene_value: Scenes.SceneName:
+	get:
+		return SceneManagerUtils.get_enum_from_string(string_value)
 
 
 ## Sets the text for the resource, which will automatically find the corresponding
 ## [Scenes.SceneName] enum.
 func set_text(text: String) -> void:
-    if string_value != text:
-        string_value = text
-        scene_value = SceneManagerUtils.get_enum_from_string(text)
-        emit_changed()
+	if string_value != text:
+		string_value = text
+		emit_changed()
 
 
 ## ToString override to print a more helpful string information for the resource.
 func _to_string() -> String:
-    return "String: {0}, Scene: {1}".format([string_value, scene_value])
+	return "String: {0}, Scene: {1}".format([string_value, scene_value])
